b8447d0cb7984c4cc90a7bdc5994ba306392d9a5
0000Blaze <rchhetry5@gmail.com>
2023-03-12 09:15:13 +0545

    error of pyttsx3 removed by removing runandwait()

--- ravitts.py
+++ ravitts.py
@@ -4,6 +4,4 @@ def textToSpeech2(text):
     filename ="response.mp3"
     engine = pyttsx3.init()
     engine.setProperty('rate', 150)
-    # engine.runAndWait()
     engine.save_to_file(text, filename)
-    engine.runAndWait()
