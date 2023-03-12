eb36b3281cd74fbd39473899d63d0f0e3cb1872c
0000Blaze <rchhetry5@gmail.com>
2023-03-12 09:26:04 +0545

    testing out female voice , not worked

--- ravitts.py
+++ ravitts.py
@@ -3,5 +3,8 @@ import pyttsx3
 def textToSpeech2(text):
     filename ="response.mp3"
     engine = pyttsx3.init()
+    # voice = engine.getProperty('voices') #get the available voices
+    # print(voice)
+    # engine.setProperty('voice', voice[1].id) #changing voice to index 1 for female voice
     engine.setProperty('rate', 150)
     engine.save_to_file(text, filename)
