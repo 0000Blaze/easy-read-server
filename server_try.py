ebe933760cb402512c91ddf0b084bf9712e36e52
NikeshDC <60224139+NikeshDC@users.noreply.github.com>
2023-03-12 10:06:23 +0545

    added helper function to run java programs

--- server_try.py
+++ server_try.py
@@ -1,3 +1,4 @@
+import subprocess
 
 def printStdoutLog(process):
     #print stdout
@@ -16,3 +17,19 @@ def printStderrLog(process):
             break
         print(line.decode('utf-8'))
 
+        
+def runJavaProgram(programName, arguments):
+    command = ["java", programName] + arguments
+    # run the Java program
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # wait for the Java program to finish
+    print("Waiting for",programName, "to finish")
+    process.wait()
+    # check the return code to see if the java process completed successfully
+    if process.returncode != 0:
+        print(programName,"failed with error code: ", process.returncode)
+        printStderrLog(process)
+        printStdoutLog(process)
+    else:
+        print(programName,"finished successfully")
+        printStdoutLog(process)
